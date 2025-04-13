62b1622b2d8ff08618adaf2ea2c389a013b4496c
gstkrsnaprnta <gstkrisnapranata@gmail.com>
2025-04-14 00:21:19 +0800

    feat: load image from file and handle failure cases

--- effects/art_sketch_effect.py
+++ effects/art_sketch_effect.py
@@ -15,4 +15,10 @@ def convert_to_art_sketch(input_path, output_path):
     Raises:
         ValueError: Jika gambar gagal dimuat atau ada kesalahan saat memproses.
     """
-    
+    try:
+        # Membaca gambar dari lokasi yang diberikan
+        image = cv2.imread(input_path)
+        
+        # Memeriksa apakah gambar berhasil dimuat
+        if image is None:
+            raise ValueError("Gagal memuat gambar. Pastikan file ada dan tidak rusak.")
