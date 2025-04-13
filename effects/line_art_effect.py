5104dbf78adf52d0eb406c035439a0c60c39df00
Restiamelia11 <restiameliaonembute@gmail.com>
2025-04-13 23:29:42 +0800

    Update line_art_effect.py

--- effects/line_art_effect.py
+++ effects/line_art_effect.py
@@ -1,1 +1,6 @@
 import cv2
+
+def resize_image(input_path, output_path, resolution='original', width=None, height=None):
+    """
+    Mengubah ukuran gambar untuk menyesuaikan kualitas saat diunduh, misalnya ke 720p atau ukuran khusus.
+    
