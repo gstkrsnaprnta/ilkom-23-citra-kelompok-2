5e56149e63bf5ba138d93bf275f5a15417bb5186
Faridsyah <159524404+parresnewbe@users.noreply.github.com>
2025-04-14 16:20:24 +0800

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -8,3 +8,13 @@ document.addEventListener('DOMContentLoaded', () => {
     // Ambil elemen dari halaman
     const uploadArea = document.getElementById('upload-area');
     const fileInput = document.getElementById('file-input');
+    const uploadButton = document.getElementById('upload-button');
+    const resultArea = document.getElementById('result-area');
+    const originalImage = document.getElementById('original-image');
+    const resultImage = document.getElementById('result-image');
+    const downloadLink = document.getElementById('download-link');
+    const messageText = document.getElementById('message-text');
+    const resolutionSelect = document.getElementById('resolution');
+    const customResDiv = document.getElementById('custom-res');
+    const widthInput = document.getElementById('width');
+    const heightInput = document.getElementById('height');
